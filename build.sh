e11497dafb2a9617e4246d34c53b5d5cef2f201d
Richard Anabeto Opoku <anabeto93@gmail.com>
2024-08-25 19:33:02 +0000

    updated.

--- build.sh
+++ build.sh
@@ -8,4 +8,6 @@ else
     echo "Google is blocked, Go proxy is enabled: GOPROXY=https://goproxy.cn,direct"
     export GOPROXY="https://goproxy.cn,direct"
 fi
+# Remove the toolchain line from go.mod if the Go version is 1.20 or 1.21
+go version | grep -q "go1.2[01]" && sed -i '/^toolchain/d' go.mod
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o server .
